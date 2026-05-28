a1a047d05473778de71c6d4a7b5ddd828e0b0bd0
sagarcde <sagar.cde999@gmail.com>
2026-05-28 17:09:12 +0530

    roboshop-setup

--- setup-mysql.sh
+++ setup-mysql.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+
 source /tmp/common.sh
 
 dnf module disable mysql -y
 
 cat >/etc/yum.repos.d/mysql.repo <<EOF
-[mysql57-community]
-name=MySQL 5.7 Community Server
-baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/x86_64/
+[mysql80-community]
+name=MySQL 8.0 Community Server
+baseurl=https://repo.mysql.com/yum/mysql-8.0-community/el/9/x86_64/
 enabled=1
 gpgcheck=0
 EOF
 
 dnf install -y mysql-community-server
 
+validate $? "MYSQL INSTALL"
+
 systemctl enable mysqld --now
+
+validate $? "MYSQL START"
 
 mysql_secure_installation --set-root-pass RoboShop@1 || true
 
-validate $? "MYSQL INSTALL"
+validate $? "MYSQL PASSWORD SET"
