e015c000848d2e7bad8d311764c980c1ce866688
sagarcde <sagar.cde999@gmail.com>
2026-05-29 10:35:46 +0530

    clearning

--- roboshop-enterprise-fixed/setup-payment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dnf install python3 gcc python3-devel unzip -y
-id roboshop || useradd roboshop
-mkdir -p /app
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
-cd /app
-rm -rf /app/*
-unzip -o /tmp/payment.zip
-pip3 install -r requirements.txt
--- roboshop-enterprise-fixed/setup-rabbitmq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server --now
-rabbitmqctl add_user roboshop RoboShop@1 || true
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
